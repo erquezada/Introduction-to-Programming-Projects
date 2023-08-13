5351bf23028cc872182cb3f738c52cbdfbad7b11
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-08-12 23:18:45 -0500

    Update CountOccurrences.java

--- CountOccurrences.java
+++ CountOccurrences.java
@@ -1,27 +1,38 @@
-import java.util.Scanner; // initialize a scanner for user input
-public class CountOccurrences{
-	public static void main(String[] args){
-		//prompt user for word or phrase or sentence
-		System.out.println("Please enter a word or phrase: ");
-		Scanner scnr = new Scanner(System.in);
-		String myStr = scnr.nextLine().toLowerCase();
-		//prompt user search for a specific letter from their word or phrase. this will return a count later
-		System.out.println("Please search for a letter: ");
-		char myLetter = scnr.next().charAt(0);
-		// this calls a method called countOccurrances 
-		int countNumberOfOccurrences = countOccurrences(myStr, myLetter);
+import java.util.Scanner;
 
-		System.out.println(countNumberOfOccurrences);
-	}
-	public static int countOccurrences (String myStr, char myLetter) {
-//this initializes a for loop that retrives a count with the number of occurrances of a letter in a word or phrase 
-		int countNumberOfOccurrences = 0;
-		for(int i = 0; i < myStr.length(); i++) {
-			if (myStr.charAt(i) == myLetter) {
-            countNumberOfOccurrences++;
-        	}
-    	}
-    	//this returns the count of occurrances and gives it back to the main method for later use
-    	return countNumberOfOccurrences;
-	}
+public class CountOccurrences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String myStr = getUserInput("Please enter a word or phrase: ", scanner);
+        char myLetter = getCharacterInput("Please search for a letter: ", scanner);
+
+        int countNumberOfOccurrences = countOccurrences(myStr, myLetter);
+
+        displayOccurrences(countNumberOfOccurrences);
+    }
+
+    public static String getUserInput(String prompt, Scanner scanner) {
+        System.out.print(prompt);
+        return scanner.nextLine().toLowerCase();
+    }
+
+    public static char getCharacterInput(String prompt, Scanner scanner) {
+        System.out.print(prompt);
+        return scanner.next().charAt(0);
+    }
+
+    public static int countOccurrences(String text, char target) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void displayOccurrences(int count) {
+        System.out.println("The letter occurs " + count + " times.");
+    }
 }
