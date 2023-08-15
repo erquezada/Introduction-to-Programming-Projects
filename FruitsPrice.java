64bc97d3e10609af4be76343ba1b69910444902e
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-08-14 21:39:37 -0500

    Update FruitsPrice.java

--- FruitsPrice.java
+++ FruitsPrice.java
@@ -1,28 +1,26 @@
-// This program runs a sample file reading fruits-database txt file based off of user input
 import java.util.Scanner;
 import java.io.*;
-
-public class FruitsPrice{
+public class FruitsPrice {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("fruits-database.txt");
+        File file = new File("grocery-store-database.txt"); // change the file name to fit your own file's name.
         Scanner scnr = new Scanner(file);
         Scanner scanner = new Scanner(System.in);
-        
-        boolean fruit = true;
-        String notFruit = "Not a fruit";
         System.out.println("Please enter a fruit name: ");
         String enteredFruit = scanner.nextLine();
-
-        while (scnr.hasNextLine()) {
+        boolean fruitFound = false;
+        for (int i = 0; scnr.hasNextLine(); i++) {
             String fruitName = scnr.next();
             double fruitPrice = scnr.nextDouble();
-            if(fruitName.equals(enteredFruit)) {
-                System.out.println(fruitName + " cost " + "$"+fruitPrice + " per pound");
-                fruit = false; 
+            if (fruitName.equals(enteredFruit)) {
+                System.out.println(fruitName + " cost $" + fruitPrice + " per pound");
+                fruitFound = true;
+                break; // Exit the loop as soon as the fruit is found
             }
         }
-        if (fruit) {
-            System.out.println(notFruit);
+        if (!fruitFound) {
+            System.out.println("Fruit not found in the database.");
         }
+        scnr.close();
+        scanner.close();
     }
 }
