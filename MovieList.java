9fee0c9a168f4b0898775636a1709f2eb1c7de79
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-08-14 21:29:28 -0500

    Update MovieList.java

--- MovieList.java
+++ MovieList.java
@@ -1,41 +1,45 @@
 import java.util.Scanner;
-import java.lang.*;
+
 public class MovieList {
-  public static void main(String[] args) {
-    boolean enjoy = false;
-    Scanner scnr = new Scanner(System.in);
-    System.out.println("What is your name?: ");
-    String userName = scnr.nextLine();
-    System.out.println(userName + ", welcome to the your favorite movie playlist.");
-    System.out.println("I see that you have no movies in your favorite movie playlist.");
-    System.out.println("Let's add some movies to your favorite movie playlist. Choose five movies to add to your favorite movie playlist.");
-    //Ask user to enter five of their favorite movies.
-    String[] movies = new String[5];
-    for (int i = 0; i < movies.length; i++) {
-      String movieName = scnr.nextLine();
-      movies[i] = movieName;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean enjoy = false;
+
+        System.out.print("What is your name?: ");
+        String userName = scanner.nextLine();
+        System.out.println(userName + ", welcome to your favorite movie playlist.");
+
+        String[] movies = new String[5];
+        System.out.println("I see that you have no movies in your favorite movie playlist.");
+        System.out.println("Let's add some movies to your favorite movie playlist. Choose five movies to add:");
+
+        for (int i = 0; i < movies.length; i++) {
+            String movieName = scanner.nextLine();
+            movies[i] = movieName;
+        }
+
+        System.out.println("There are " + movies.length + " movies in your favorite movie playlist:");
+        for (String movie : movies) {
+            System.out.println(movie);
+        }
+
+        System.out.print("What movie would you like to watch? ");
+        String pickMovie = scanner.nextLine();
+        
+        for (int i = 0; i < movies.length; i++) {
+            if (movies[i].equals(pickMovie)) {
+                System.out.println("This movie is listed as #" + (i + 1) + " in your list.");
+                enjoy = true;
+                break; // Exit loop since movie is found
+            }
+        }
+
+        if (enjoy) {
+            System.out.println("Enjoy your movie, " + userName + ".");
+        } else {
+            System.out.println("Sorry, " + userName + ", this movie is unavailable.");
+        }
+
+        scanner.close();
     }
-    System.out.println("There are 5 movies in your favorite movie playlist");
-    for (int i = 0; i < movies.length; i++) {
-      // Display the entered movies to user
-      System.out.println(movies[i]);
-    }
-    // Ask user to pick a movie to watch
-    System.out.println("What movie would you like to watch?");
-    String pickMovie = scnr.nextLine();
-    for (int i = 0; i < movies.length; i++) {
-      if (movies[i].equals(pickMovie)) {
-        System.out.println("This movie is listed as #" + (i+1) + " in your list");
-        enjoy = true;
-      } // Enjoy your movie 
-      else {}
-    }
-    if (enjoy == true) {
-      System.out.println("Enjoy your movie, " + userName + ".");
-      System.exit(1);
-    } else if (enjoy == false) {
-      System.out.println("Sorry," + userName + " this movie is unavailable.");
-      //Sorry this movie is not available
-    }
-  }
 }
