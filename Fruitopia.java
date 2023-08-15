66ec539bd5a777cd2384dbfaba465fe9002d5258
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-08-14 22:03:00 -0500

    Update GroveryStore.java

--- Fruitopia.java
+++ Fruitopia.java
@@ -1,151 +1,120 @@
-// This file runs an interactive program called Fruitopia
 import java.io.*;
 import java.util.Scanner;
-import java.lang.*;
-public class Fruitopia  {
-  public static void main(String[] args) throws FileNotFoundException {
-    File file = new File("fruits-database.txt");
-    Scanner scnr = new Scanner(file);
-    Scanner scanner = new Scanner(System.in);
-
-    //Declare variables
-    boolean fruit = true;
-    boolean option = true;
-    double totalPrice = 0;
-    double totalPounds = 0;
-    double numberOfBagsUserHasToBuy = 0;
-    double costOfBags = 0.15;
-    double giftCardAmount = 0;
-    //Home screen
-    // Write a welcome message that displays the name of the store to the user
-    // Ask user to select an option from a menu
-    // start switch statement
-    while (true) {
-      System.out.println("Welcome to Fruitopia");
-      System.out.println("Please select an option from the menu!");
-      System.out.println("1. " + "Add item");
-      System.out.println("2. " + "View cart");
-      System.out.println("3. " + "Clear cart");
-      System.out.println("4. " + "Check out");
-      System.out.println("5. " + "Exit");
-
-      int switchStarter = scanner.nextInt();
-      switch (switchStarter) {
-
-        //If user selects option 1
-      case 1:
-        System.out.println("1." + "Add item");
-        System.out.println("Please select a fruit");
-        System.out.println();
-        scnr = new Scanner(file);
-        while (scnr.hasNextLine()) {
-
-          String fruitName = scnr.next();
-          double fruitPrice = scnr.nextDouble();
-          System.out.println("Fruit: " + fruitName);
-          System.out.println("Price: " + fruitPrice);
-          System.out.println();
-        }
-        // Ask user for the name of the fruit they would like to purchase
-        scanner = new Scanner(System.in);
-        System.out.println("Enter the name of the fruit you would like to purchase.");
-        String enteredFruit = scanner.nextLine();
-        scnr = new Scanner(file);
-        while (scnr.hasNextLine()) {
-          String fruitName1 = scnr.next();
-          double fruitPrice1 = scnr.nextDouble();
-
-          if (fruitName1.equals(enteredFruit)) {
-            System.out.println("How many pounds of this fruit are you adding?");
-            System.out.println("Enter a whole number greater than zero.");
-            double pounds = scanner.nextDouble();
-            if(pounds <= 0) {
-              System.err.println("Enter a whole number greater than zero.");
-              break;
+public class GroceryStore {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("fruits-database.txt");
+        Scanner scanner = new Scanner(System.in);
+        double totalPrice = 0;
+        double totalPounds = 0;
+        double costOfBags = 0.15;
+        double giftCardAmount = 0;
+        while (true) {
+            printMenu();
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addItem(file, scanner, totalPounds, totalPrice);
+                    break;
+                case 2:
+                    viewCart(totalPounds, totalPrice);
+                    break;
+                case 3:
+                    clearCart(totalPounds, totalPrice);
+                    break;
+                case 4:
+                    checkout(scanner, totalPounds, totalPrice, costOfBags, giftCardAmount);
+                    break;
+                case 5:
+                    exit();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select an option from the menu.");
             }
-            totalPounds = totalPounds + pounds;
-            totalPrice = totalPrice + (pounds * fruitPrice1);
-            fruit = false;
-          }
         }
-        if (fruit) {
-          System.out.println("Fruit not available");
-          System.out.println("Enter the name of the fruit you would like to purchase.");
+    }
+    public static void printMenu() {
+        System.out.println("Welcome to the Grocery Store");
+        System.out.println("Please select an option from the menu:");
+        System.out.println("1. Add item");
+        System.out.println("2. View cart");
+        System.out.println("3. Clear cart");
+        System.out.println("4. Checkout");
+        System.out.println("5. Exit");
+    }
+    public static void addItem(File file, Scanner scanner, double totalPounds, double totalPrice) throws FileNotFoundException {
+        System.out.println("Please select a fruit:");
+        try (Scanner scnr = new Scanner(file)) {
+            while (scnr.hasNextLine()) {
+                String fruitName = scnr.next();
+                double fruitPrice = scnr.nextDouble();
+                System.out.println(fruitName + " - $" + fruitPrice);
+            }
         }
-        fruit = true;
-
-        // Display current total weight and current total price
-        System.out.println("Your current total weight is: " + totalPounds + " lbs and total price is: $" + totalPrice);
-        break;
-
-        //If user selects option 2:
-      case 2:
-        System.out.println("2." + "View cart");
-        //Create an array to list items inside of cart for user
-        // Display user cart with current total pounds and total price
-        System.out.println("Your current total weight is: " + totalPounds + " lbs and total price is: $ " + totalPrice);
-        //System.out.println(totalPounds + totalPrice);
-        break;
-
-        //If user selects option 3:
-      case 3:
-        System.out.println("3." + "Clear cart");
-
-        totalPounds *= 0;
-        totalPrice *= 0;
-
-        // This empties the users cart
-        System.out.println("Your current total weight is: " + totalPounds + " lbs and total price is: $" + totalPrice);
-        break;
-
-        //If user selects option 4:
-        case 4: 
-          System.out.println("Your current total weight is: " + totalPounds + " lbs" + "\nYour current total price is: $"+ totalPrice);
-          System.out.println("Due to a bag shortage, Fruitopia does not give out bags for free.");
-          System.out.println("We encourage our customers to bring their own bags.");
-          System.out.println("How many bags did you bring?");
-          System.out.println("Each bag can hold up to five pounds.");
-          System.out.println("Enter a whole number greater than zero.");
-          double numberOfBagsUserBrought = scanner.nextDouble();
-          numberOfBagsUserHasToBuy = (double) (Math.ceil(totalPounds / 5) - numberOfBagsUserBrought) + (totalPounds % 5);
-          if(numberOfBagsUserBrought < 0) {
-              System.err.println("Enter a whole number greater than zero.");
-              break;
-          }
-          totalPrice += numberOfBagsUserHasToBuy *0.15;
-          System.out.println("Your cart's total weight is: "+ totalPounds + " lbs");
-          System.out.println("So you need to purchase "+ numberOfBagsUserHasToBuy + " bags."); 
-          System.out.println("If you did not bring enough bags, we will supply you with bags for a fee."); 
-          System.out.println("additional bags which cost: "); 
-          System.out.println(costOfBags + " cents."); 
-          System.out.println("Your total is: $"+ totalPrice);
-          System.out.print("Enter gift card amount: ");
-          giftCardAmount = scanner.nextDouble();
-          if (totalPrice>giftCardAmount) {
+        scanner.nextLine(); // Consume newline
+        System.out.println("Enter the name of the fruit you want to purchase:");
+        String enteredFruit = scanner.nextLine();
+        try (Scanner scnr = new Scanner(file)) {
+            boolean found = false;
+            while (scnr.hasNextLine()) {
+                String fruitName = scnr.next();
+                double fruitPrice = scnr.nextDouble();
+                if (fruitName.equalsIgnoreCase(enteredFruit)) {
+                    System.out.println("Enter the number of pounds:");
+                    double pounds = scanner.nextDouble();
+                    if (pounds <= 0) {
+                        System.err.println("Please enter a valid weight.");
+                        break;
+                    }
+                    totalPounds += pounds;
+                    totalPrice += pounds * fruitPrice;
+                    System.out.println("Added " + pounds + " lbs of " + fruitName + " to cart.");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.println("Fruit not available.");
+            }
+        }
+    }
+    public static void viewCart(double totalPounds, double totalPrice) {
+        System.out.println("Items in cart:");
+        System.out.printf("Total weight: %.2f lbs | Total price: $%.2f%n", totalPounds, totalPrice);
+    }
+    public static void clearCart(double totalPounds, double totalPrice) {
+        totalPounds = 0;
+        totalPrice = 0;
+        System.out.println("Cart cleared.");
+    }
+    public static void checkout(Scanner scanner, double totalPounds, double totalPrice, double costOfBags, double giftCardAmount) {
+        System.out.printf("Total weight: %.2f lbs | Total price: $%.2f%n", totalPounds, totalPrice);
+        System.out.println("Due to a bag shortage, we encourage customers to bring their own bags.");
+        System.out.println("How many bags did you bring? Each bag can hold up to 5 lbs.");
+        double bagsBrought = scanner.nextDouble();
+        double bagsNeeded = Math.ceil(totalPounds / 5) - bagsBrought;
+        if (bagsNeeded < 0) {
+            bagsNeeded = 0;
+        }
+        double bagCost = bagsNeeded * costOfBags;
+        double finalTotal = totalPrice + bagCost;
+        System.out.println("You need to purchase " + bagsNeeded + " bags.");
+        System.out.println("Additional bags cost: $" + bagCost);
+        System.out.println("Your new total is: $" + finalTotal);
+        System.out.print("Enter gift card amount: ");
+        giftCardAmount = scanner.nextDouble();
+        if (finalTotal > giftCardAmount) {
             System.err.println("Insufficient funds!");
-          } else {
-            giftCardAmount-=totalPrice;
-            System.out.println("Thank you for shopping at Fruitopia, come back soon!\n" + "Remaing gift card balance: $"+ giftCardAmount);
-            totalPrice = 0;
+        } else {
+            giftCardAmount -= finalTotal;
+            System.out.println("Thank you for shopping at the Grocery Store!");
+            System.out.println("Remaining gift card balance: $" + giftCardAmount);
             totalPounds = 0;
-          }
-          break;
-
-        //If user selects option 5:
-      case 5:
-        System.out.println("5." + "Exit");
-        System.out.println("Thank you for shopping at Fruitopia");
+            totalPrice = 0;
+        }
+    }
+    public static void exit() {
+        System.out.println("Thank you for shopping at the Grocery Store");
         System.out.println("Have a great day!");
         System.exit(0);
-        break;
-
-      default:
-        System.out.println("Please select an option from the menu!");
-        System.out.println("Thank you for shopping at Fruitopia");
-        System.out.println("Have a great day!");
-
-        break;
-      }
     }
-  }
 }
